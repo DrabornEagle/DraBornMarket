8d07f569186140b359337db3ff74b1d68e63e058
DrabornEagle <draborneagle@gmail.com>
2026-08-03 10:22:13 +0300

    Safe area sağlayıcısını etkinleştir

--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
+import React from 'react';
+import { LogBox } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 import App from './App';
 
-registerRootComponent(App);
+LogBox.ignoreLogs([
+  "SafeAreaView has been deprecated and will be removed in a future release",
+]);
+
+function DkdRoot() {
+  return React.createElement(
+    SafeAreaProvider,
+    { initialMetrics: initialWindowMetrics },
+    React.createElement(App),
+  );
+}
+
+registerRootComponent(DkdRoot);
